6ac20b400708c572b661dda00f058a829e6d047c
Will Norton <willnorton@Wills-MacBook-Pro.local>
2020-01-27 15:46:32 -0600

    Added 2 files and worked with the csv files

--- csv_temps_2.py
+++ csv_temps_2.py
@@ -13,15 +13,22 @@ for index,column_header in enumerate(header_row):
     print(index, column_header)
 
 highs = [] 
+dates = []
 
 for row in csv_file:
     highs.append(int(row[5]))
+    dates.append(row[2])
 
-print(highs[:10])
+#print(highs[:10])
 
-plt.plot(highs,color='red')
+fig = plt.figure()
+
+plt.plot(dates, highs, color='red')
 plt.title("Daily High Temps, July 2018", fontsize=16)
-plt.xlabel("", fontsize=16)
-plt.ylabel("Temperature (F)", fontsize=16)
-plt.tick_params(axis="both", which="major", labelsize=16)
+plt.xlabel("", fontsize=10)
+plt.ylabel("Temperature (F)", fontsize=12)
+plt.tick_params(axis="both", which="major", labelsize=12)
+
+fig.autofmt_xdate()
+
 plt.show()
